620cc0aae06920fe1da96fd69bea55cc518f2676
mmrakib <mayaz@rakib.com.au>
2024-12-25 23:22:56 +1100

    Updated program entry point to work with homepage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,32 @@
-function App() {
+import React, { useState } from "react";
+import { MantineProvider } from "@mantine/core";
 
+import "./global.scss";
+
+import Navbar from "./components/navigation/navbar.jsx";
+import Introduction from "./components/about/introduction.jsx";
+import About from "./components/about/about.jsx";
+
+function App() {
+    // Controls whether the About component should be shown
+    const [showAbout, setShowAbout] = useState(false);
+
+    // This callback is triggered when Introduction finishes
+    const handleIntroductionComplete = () => {
+        setShowAbout(true);
+    };
+
+    return (
+        <MantineProvider>
+            <Navbar />
+
+            {/* Pass the callback to Introduction via onComplete prop */}
+            <Introduction onComplete={handleIntroductionComplete} />
+
+            {/* Conditionally render <About /> only after Introduction completes */}
+            {showAbout && <About />}
+        </MantineProvider>
+    );
 }
 
 export default App;
