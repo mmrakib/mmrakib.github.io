93ce017b1404ee5ff1a50e312c27e1b4337a6fa0
mmrakib <mayaz@rakib.com.au>
2024-12-27 22:00:54 +1100

    Added routing path for blog.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./components/navigation/navbar.jsx";
 import HomePage from "./components/pages/homepage.jsx";
 // /import ProjectsPage from "./components/pages/projectspage.jsx";
 import WritingPage from "./components/pages/writingpage.jsx";
+import BlogPage from "./components/pages/blogpage.jsx";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 {/*<Route path="/projects" element={<ProjectsPage />} />}*/}
                 <Route path="/writing" element={<WritingPage />} />
+                <Route path="/post/:id" element={<BlogPage />} />
             </Routes>
         </Router>
     );
